43e8863ba40fbeb18ce846af5a0dd2a99ee2da79
Николай Сафонов <nasafonov@edu.hse.ru>
2018-06-12 20:54:58 +0300

    Логика.

---
+++ Hleb.Classes/Model/Ingredients.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hleb.Classes.Model
+{
+    class Ingredients
+    {
+    }
+}
