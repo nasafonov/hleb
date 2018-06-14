fd0a7639e1a459caf4f4c7a7a471070e9134e0e3
Егор Низов <eenizov@edu.hse.ru>
2018-06-14 23:32:42 +0300

    Добавил возможность поставить аватарку

--- Hleb/UserWindow.xaml.cs
+++ Hleb/UserWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,10 +9,16 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
+
+
 using System.Windows.Shapes;
+
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Windows.Media;
 
 namespace Hleb
 {
@@ -19,6 +27,8 @@ namespace Hleb
     /// </summary>
     public partial class UserWindow : Window
     {
+        private byte[] data;
+
         public UserWindow()
         {
             InitializeComponent();
@@ -61,6 +71,36 @@ namespace Hleb
             var fw = new FavouritesWindow();
             fw.Show();
             Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                OpenFileDialog fileDialog = new OpenFileDialog();
+                fileDialog.ShowDialog();
+                //создание диалогового окна для выбора файла
+                fileDialog.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*"; //формат загружаемого файла
+                string way = fileDialog.FileName;
+                Image img = new Image();
+                ImageSourceConverter imgs = new ImageSourceConverter();
+                userPhoto.Source = null;
+                userPhoto.SetValue(Image.SourceProperty, imgs.ConvertFromString(way));
+            }
+            catch
+            {
+                System.Windows.Forms.MessageBox.Show("Something Went Wrong");
+            }
+
+
+
+
+           
+           
+
+
+            
         }
     }
 }
